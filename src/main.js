c6262c1a29a88b58bd844631392f624ec34d74fc
Mohammad Rahmani <rto1680@gmail.com>
2021-01-04 15:29:59 +0430

    Add and config axios

--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { createApp } from 'vue'
+import axios from 'axios'
 import App from './App.vue'
+import { createApp } from 'vue'
+import VueAxios from 'vue-axios'
 
-createApp(App).mount('#app')
+// Application env
+window.env = require("../env.js");
+
+const app = createApp(App)
+
+// Create a new axios instance with custom configuration
+const instance = axios.create({
+    baseURL: env.BASE_URL,
+    headers: {
+        'Accept-Version': 'v1',
+        'Authorization': `Client-ID ${env.ACCESS_KEY}`
+    }
+});
+
+window.axios = instance
+
+app.use(VueAxios, axios)
+app.mount('#app')
+
