546cd92138c24630392195aa470ce8fac1d42713
Mohammad Rahmani <rto1680@gmail.com>
2021-01-04 22:16:35 +0430

    Remove unused code

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
 module.exports = {
     publicPath: '',
-    css: {
-        loaderOptions: {
-            sass: {
-                prependData: `@import "assets/scss/utils/main.scss";`
-            }
-        }
-    }
 }
